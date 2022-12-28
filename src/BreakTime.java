e3e9ff092e7940e72567d2c474731648f70c6ebc
richbabushka <latatsvt@gmail.com>
2022-12-28 15:13:11 +0800

    added breaktime in the book

--- src/BreakTime.java
+++
@@ -1,6 +0,0 @@
-public class BreakTime {
-
-
-   // it is a breaks time, time to do something fun
-    // reading a book
-}
--- src/webapp/BreakTime.java
+++ src/webapp/BreakTime.java
@@ -1,9 +1,7 @@
 package webapp;
 
 public class BreakTime {
-
-    // break time
-    //  to watch movie
-    // watching the new action movie
+    // time
+    //  to read a book
 
 }
